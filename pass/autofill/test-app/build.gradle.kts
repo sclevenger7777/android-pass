6d735d4bea9696fc4ac449aab3d81644edf59469
Antoine Marchaud <antoine.marchaud@external.proton.ch>
2026-06-10 15:28:32 +0200

    chore(release): version 1.39.2 → 1.40.0

--- pass/autofill/test-app/build.gradle.kts
+++ pass/autofill/test-app/build.gradle.kts
@@ -23,7 +23,7 @@ plugins {
     alias(libs.plugins.gradlePlugin.compose.compiler)
 }
 
-val appVersionName = "1.39.2"
+val appVersionName = "1.40.0"
 val appVersionCode = versionCode(appVersionName)
 val archivesBaseName = "AutofillTestApp-$appVersionName"
 
